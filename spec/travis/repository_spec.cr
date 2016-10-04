12ccba54f9320d5a8427ae7ef01119dc074c467c
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-04 11:40:11 -0400

    Pass key in the all request to filter data

--- spec/travis/repository_spec.cr
+++ spec/travis/repository_spec.cr
@@ -6,5 +6,13 @@ describe Travis::Repository do
       repositories = Travis::Repository.all
       (repositories.size).should eq(25)
     end
+
+    context "when param specified" do
+      it "returns an array of repositories filtered by the param" do
+        repositories = Travis::Repository.all("glossier")
+        repository = repositories.first
+        (repository.slug).should eq("glossier/demo_segment")
+      end
+    end
   end
 end
--- src/travis/resource.cr
+++ src/travis/resource.cr
@@ -2,15 +2,15 @@ require "./resource"
 
 module Travis
   class Resource
-    def self.all
-      response = Travis::Client.request(@@resource)
+    def self.all(key : String? = nil)
+      resource = key ? "#{@@resource}/#{key}" : @@resource
+      response = Travis::Client.request(resource)
 
       unless response.success?
         raise Exceptions::Generic.new("Something went wrong")
       end
 
       Array(Travis::Repository).from_json(response.body)
-      # response.body.map! { |element| from_json(element) }
     end
   end
 end
