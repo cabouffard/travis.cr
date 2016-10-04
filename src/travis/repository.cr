4a9d32d3a3f14f820c5a5009ed892222743b5b22
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-04 13:54:40 -0400

    Change the JSON representation of repository
    The documentation of Travis is bonker, the fields that are supposed to
    be returned are not and some of them have different name. This commit
    reflects the proper returned values.

--- src/travis/repository.cr
+++ src/travis/repository.cr
@@ -5,16 +5,17 @@ module Travis
     @@resource = "repos"
 
     JSON.mapping({
-      id: Int16,
+      id: Int32,
       slug: String,
       description: {type: String, nilable: true},
-      last_build_id: {type: Int16, nilable: true},
+      last_build_id: {type: Int32, nilable: true},
       last_build_number: {type: String, nilable: true},
-      last_build_state:  {type: String, nilable: true},
-      last_build_duration: {type: Int16, nilable: true},
+      last_build_state:  {type: Int16, nilable: true},
+      last_build_result:  {type: Int16, nilable: true},
+      last_build_duration: {type: Int32, nilable: true},
+      last_build_language: {type: Int32, nilable: true},
       last_build_started_at: {type: Time, nilable: true, converter: Time::Format.new("%F")},
       last_build_finished_at: {type: Time, nilable: true, converter: Time::Format.new("%F")},
-      github_language: {type: String, nilable: true}
     })
   end
 end
